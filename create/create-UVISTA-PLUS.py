4328d9561ba65561274ba75db85d7483381caa9c
Nelson Nunes <nelsonnunes@me.com>
2025-01-17 10:58:30 -0500

    Various changes in support of target tools

--- create/create-UVISTA-PLUS.py
+++ create/create-UVISTA-PLUS.py
@@ -17,91 +17,91 @@ print('Loading UltraVISTA catalog...', flush=True)
 print('Matching zCOSMOS-Bright...', flush=True)
 (catalog_ZCB, matches_ZCB) = create.cross_match_ZCB(catalog_UVISTA)
 print(f"  Matched = {matches_ZCB.count} sources", flush=True)
-catalog.close(catalog_ZCB)
+catalog_ZCB.close()
 del catalog_ZCB, matches_ZCB
 
 # Match zCOSMOS-Deep
 print('Matching zCOSMOS-Deep...', flush=True)
 (catalog_ZCD, matches_ZCD) = create.cross_match_ZCD(catalog_UVISTA)
 print(f"  Matched = {matches_ZCD.count} sources", flush=True)
-catalog.close(catalog_ZCD)
+catalog_ZCD.close()
 del catalog_ZCD, matches_ZCD
 
 # Match 3D-HST
 print('Matching 3D-HST...', flush=True)
 (catalog_3DHST, matches_3DHST) = create.cross_match_3DHST(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_3DHST.count} sources", flush=True)
-catalog.close(catalog_3DHST)
+catalog_3DHST.close()
 del catalog_3DHST, matches_3DHST
 
 # Match VUDS
 print('Matching VUDS...', flush=True)
 (catalog_VUDS, matches_VUDS) = create.cross_match_VUDS(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_VUDS.count} sources", flush=True)
-catalog.close(catalog_VUDS)
+catalog_VUDS.close()
 del catalog_VUDS, matches_VUDS
 
 # Match Casey DSFG
 print('Matching Casey DSFG...', flush=True)
 (catalog_Casey, matches_Casey) = create.cross_match_Casey(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_Casey.count} sources", flush=True)
-catalog.close(catalog_Casey)
+catalog_Casey.close()
 del catalog_Casey, matches_Casey
 
 # Match DEIMOS 10K
 print('Matching DEIMOS 10K...', flush=True)
 (catalog_DEIMOS, matches_DEIMOS) = create.cross_match_DEMOS(catalog_UVISTA)
 print(f"  Matched = {matches_DEIMOS.count} sources", flush=True)
-catalog.close(catalog_DEIMOS)
+catalog_DEIMOS.close()
 del catalog_DEIMOS, matches_DEIMOS
 
 # Match MOSDEF
 print('Matching MOSDEF...', flush=True)
 (catalog_MOSDEF, matches_MOSDEF) = create.cross_match_MOSDEF(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_MOSDEF.count} sources", flush=True)
-catalog.close(catalog_MOSDEF)
+catalog_MOSDEF.close()
 del catalog_MOSDEF, matches_MOSDEF
 
 # Match FMOS
 print('Matching FMOS...', flush=True)
 (catalog_FMOS, matches_FMOS) = create.cross_match_FMOS(catalog_UVISTA)
 print(f"  Matched = {matches_FMOS.count} sources", flush=True)
-catalog.close(catalog_FMOS)
+catalog_FMOS.close()
 del catalog_FMOS, matches_FMOS
 
 # Match KMOS3D
 print('Matching KMOS3D...', flush=True)
 (catalog_KMOS3D, matches_KMOS3D) = create.cross_match_KMOS3D(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_KMOS3D.count} sources", flush=True)
-catalog.close(catalog_KMOS3D)
+catalog_KMOS3D.close()
 del catalog_KMOS3D, matches_KMOS3D
 
 # Match C3R2
 print('Matching C3R2...', flush=True)
 (catalog_C3R2, matches_C3R2) = create.cross_match_C3R2(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_C3R2.count} sources", flush=True)
-catalog.close(catalog_C3R2)
+catalog_C3R2.close()
 del catalog_C3R2, matches_C3R2
 
 # Match Lega-C
 print('Matching LEGA-C...', flush=True)
 (catalog_LEGAC, matches_LEGAC) = create.cross_match_LEGAC(catalog_UVISTA)
 print(f"  Matched = {matches_LEGAC.count} sources", flush=True)
-catalog.close(catalog_LEGAC)
+catalog_LEGAC.close()
 del catalog_LEGAC, matches_LEGAC
 
 # Match HSCSSP
 print('Matching HSCSSP...', flush=True)
 (catalog_HSCSSP, matches_HSCSSP) = create.cross_match_HSCSSP(catalog_UVISTA, 'COSMOS')
 print(f"  Matched = {matches_HSCSSP.count} sources", flush=True)
-catalog.close(catalog_HSCSSP)
+catalog_HSCSSP.close()
 del catalog_HSCSSP, matches_HSCSSP
 
 # Match DESI
 print('Matching DESI...', flush=True)
 (catalog_DESI, matches_DESI) = create.cross_match_DESI(catalog_UVISTA, 'COSMOS', catalog_region_UVISTA, catalog_params_UVISTA.wcs)
 print(f"  Matched = {matches_DESI.count} sources", flush=True)
-catalog.close(catalog_DESI)
+catalog_DESI.close()
 del catalog_DESI, matches_DESI
 
 # Save UltraVISTA Matched
--- create/create.py
+++ create/create.py
@@ -9,26 +9,26 @@ from survey_tools import catalog, match
 
 def load_3DHST(field):
     catalog_params_3DHST = catalog.get_params('3D-HST', field, 'F160W')
-    catalog_3DHST = catalog.read(catalog_params_3DHST, force_tables=True)
+    catalog_3DHST = catalog.CatalogData(catalog_params_3DHST, force_tables=True)
     catalog_region_3DHST = catalog.get_catalog_region(catalog_3DHST, added_distance=0.5*u.degree)
     return (catalog_params_3DHST, catalog_3DHST, catalog_region_3DHST)
 
 def load_UVISTA():
     catalog_params_UVISTA = catalog.get_params('UVISTA', 'COSMOS', 'K')
-    catalog_UVISTA = catalog.read(catalog_params_UVISTA)
+    catalog_UVISTA = catalog.CatalogData(catalog_params_UVISTA)
     catalog_region_UVISTA = catalog.get_catalog_region(catalog_UVISTA)
     return (catalog_params_UVISTA, catalog_UVISTA, catalog_region_UVISTA)
 
 def cross_match_3DHST(catalog_master, field):
     catalog_params_3DHST = catalog.get_params('3D-HST', field, 'F160W')
-    catalog_3DHST = catalog.read(catalog_params_3DHST)
+    catalog_3DHST = catalog.CatalogData(catalog_params_3DHST)
     matches_3DHST = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_3DHST)
     match.append_cross_matched_data(matches_3DHST, catalog_master, catalog_3DHST)
     return (catalog_3DHST, matches_3DHST)
 
 def cross_match_ZCB(catalog_master):
     catalog_params_ZCB = catalog.get_params('ZCOSMOS-BRIGHT')
-    catalog_ZCB = catalog.read(catalog_params_ZCB)
+    catalog_ZCB = catalog.CatalogData(catalog_params_ZCB)
     if catalog_master.catalog == 'UVISTA':
         matches_ZCB = match.cross_match_catalogs_by_id_ra_dec(catalog_master, catalog_ZCB, id_field1='z_spec_id')
     else:
@@ -38,35 +38,35 @@ def cross_match_ZCB(catalog_master):
 
 def cross_match_ZCD(catalog_master):
     catalog_params_ZCD = catalog.get_params('ZCOSMOS-DEEP')
-    catalog_ZCD = catalog.read(catalog_params_ZCD)
+    catalog_ZCD = catalog.CatalogData(catalog_params_ZCD)
     matches_ZCD = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_ZCD)
     match.append_cross_matched_data(matches_ZCD, catalog_master, catalog_ZCD)
     return (catalog_ZCD, matches_ZCD)
 
 def cross_match_VUDS(catalog_master, field):
     catalog_params_VUDS = catalog.get_params('VUDS', field)
-    catalog_VUDS = catalog.read(catalog_params_VUDS)
+    catalog_VUDS = catalog.CatalogData(catalog_params_VUDS)
     matches_VUDS = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_VUDS)
     match.append_cross_matched_data(matches_VUDS, catalog_master, catalog_VUDS)
     return (catalog_VUDS, matches_VUDS)
 
 def cross_match_Casey(catalog_master, field):
     catalog_params_Casey = catalog.get_params('Casey', field)
-    catalog_Casey = catalog.read(catalog_params_Casey)
+    catalog_Casey = catalog.CatalogData(catalog_params_Casey)
     matches_Casey = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_Casey)
     match.append_cross_matched_data(matches_Casey, catalog_master, catalog_Casey)
     return (catalog_Casey, matches_Casey)
 
 def cross_match_DEMOS(catalog_master):
     catalog_params_DEIMOS = catalog.get_params('DEIMOS')
-    catalog_DEIMOS = catalog.read(catalog_params_DEIMOS)
+    catalog_DEIMOS = catalog.CatalogData(catalog_params_DEIMOS)
     matches_DEIMOS = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_DEIMOS)
     match.append_cross_matched_data(matches_DEIMOS, catalog_master, catalog_DEIMOS)
     return (catalog_DEIMOS, matches_DEIMOS)
 
 def cross_match_MOSDEF(catalog_master, field):
     catalog_params_MOSDEF = catalog.get_params('MOSDEF', field)
-    catalog_MOSDEF = catalog.read(catalog_params_MOSDEF)
+    catalog_MOSDEF = catalog.CatalogData(catalog_params_MOSDEF)
     if catalog_master.catalog == '3DHST':
         matches_MOSDEF = match.cross_match_catalogs_by_id(catalog_master, catalog_MOSDEF, id_field2='ID_V4')
     else:
@@ -76,14 +76,14 @@ def cross_match_MOSDEF(catalog_master, field):
 
 def cross_match_FMOS(catalog_master):
     catalog_params_FMOS = catalog.get_params('FMOS')
-    catalog_FMOS = catalog.read(catalog_params_FMOS)
+    catalog_FMOS = catalog.CatalogData(catalog_params_FMOS)
     matches_FMOS = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_FMOS)
     match.append_cross_matched_data(matches_FMOS, catalog_master, catalog_FMOS)
     return (catalog_FMOS, matches_FMOS)
 
 def cross_match_KMOS3D(catalog_master, field):
     catalog_params_KMOS3D = catalog.get_params('KMOS3D', field)
-    catalog_KMOS3D = catalog.read(catalog_params_KMOS3D)
+    catalog_KMOS3D = catalog.CatalogData(catalog_params_KMOS3D)
     if catalog_master.catalog == '3DHST':
         matches_KMOS3D = match.cross_match_catalogs_by_id(catalog_master, catalog_KMOS3D)
     else:
@@ -94,14 +94,14 @@ def cross_match_KMOS3D(catalog_master, field):
 
 def cross_match_C3R2(catalog_master, field):
     catalog_params_C3R2 = catalog.get_params('C3R2', field)
-    catalog_C3R2 = catalog.read(catalog_params_C3R2)
+    catalog_C3R2 = catalog.CatalogData(catalog_params_C3R2)
     matches_C3R2 = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_C3R2)
     match.append_cross_matched_data(matches_C3R2, catalog_master, catalog_C3R2)
     return (catalog_C3R2, matches_C3R2)
 
 def cross_match_LEGAC(catalog_master):
     catalog_params_LEGAC = catalog.get_params('LEGAC')
-    catalog_LEGAC = catalog.read(catalog_params_LEGAC)
+    catalog_LEGAC = catalog.CatalogData(catalog_params_LEGAC)
     if catalog_master.catalog == 'UVISTA':
         matches_LEGAC = match.cross_match_catalogs_by_id_ra_dec(catalog_master, catalog_LEGAC, id_field2='ID')
     else:
@@ -111,14 +111,14 @@ def cross_match_LEGAC(catalog_master):
 
 def cross_match_HSCSSP(catalog_master, field):
     catalog_params_HSCSSP = catalog.get_params('HSCSSP', field)
-    catalog_HSCSSP = catalog.read(catalog_params_HSCSSP)
+    catalog_HSCSSP = catalog.CatalogData(catalog_params_HSCSSP)
     matches_HSCSSP = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_HSCSSP)
     match.append_cross_matched_data(matches_HSCSSP, catalog_master, catalog_HSCSSP)
     return (catalog_HSCSSP, matches_HSCSSP)
 
 def cross_match_DESI(catalog_master, field, region, region_wcs):
     catalog_params_DESI = catalog.get_params('DESI', field)
-    catalog_DESI = catalog.read(catalog_params_DESI, region=region, region_wcs=region_wcs)
+    catalog_DESI = catalog.CatalogData(catalog_params_DESI, region=region, region_wcs=region_wcs)
     matches_DESI = match.cross_match_catalogs_by_ra_dec(catalog_master, catalog_DESI)
     match.append_cross_matched_data(matches_DESI, catalog_master, catalog_DESI)
     return (catalog_DESI, matches_DESI)
